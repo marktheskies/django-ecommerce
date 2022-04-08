cbc20a5c1c540a64d6f61e2879e9ea03ddaf5d2d
Mark Harris <29774695+marktheskies@users.noreply.github.com>
2022-04-08 14:47:24 +1000

    Implement Product and Category admin

--- backend/ecommerce/admin.py
+++ backend/ecommerce/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
